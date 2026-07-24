473f039f4afd23d43606a55cb0736859ddafd608
kota <52403688+yashikota@users.noreply.github.com>
2026-07-24 16:55:02 +0900

    chore: update enbu-desktop cask to v0.7.5

--- Casks/enbu-desktop.rb
+++ Casks/enbu-desktop.rb
@@ -1,6 +1,6 @@
 cask "enbu-desktop" do
-  version "0.7.7"
-  sha256 "3f6c31999176e39a0afaf05945e8cacf97c92fd83ce6b2d806254ef584e76bb5"
+  version "0.7.5"
+  sha256 "3b036051f45f4dd39b4e2651c17fce9e5c92a57bd25b084d93ad8f7ea6a954f8"
 
   url "https://github.com/enbu-net/enbu/releases/download/v#{version}/enbu-desktop_v#{version}_darwin_arm64.dmg"
   name "enbu"
